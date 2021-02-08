32bfd661f3ba0843b810427bfd16641b410f150a
sia <anastasiya6956@mail.ru>
2021-02-08 19:24:58 +0300

    final version

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,8 @@
-let education = document.querySelector('.right-col__education');
-let educationContent = document.querySelector('.right-col__education .content');
+let main = document.querySelector('main');
 
-education.addEventListener('click', function(event) {
+main.addEventListener('click', function(event) {
     if (event.target.tagName != 'H3') return;
-    educationContent.classList.toggle('hide')
-    // for(let item of educationContent) {
-    //     item.classList.toggle('hide')
-    // }    
-})
-
-
-let work = document.querySelector('.right-col__work');
-let workContent = document.querySelector('.right-col__work .content');
-
-work.addEventListener('click', function(event) {
-    if (event.target.tagName != 'H3') return;
-    workContent.classList.toggle('hide')
-    // for(let item of workContent) {
-    //     item.classList.toggle('hide')
-    // }
-})
-
-
-let skills = document.querySelector('.right-col__skills');
-let skillsContent = document.querySelector('.right-col__skills .content');
-
-skills.addEventListener('click', function(event) {
-    if (event.target.tagName != 'H3') return;
-    skillsContent.classList.toggle('hide')
-    // for(let item of skillsContent) {
-    //     item.classList.toggle('hide')
-    // }
+    event.target.nextElementSibling.classList.toggle('hide')
+      
 })
 
